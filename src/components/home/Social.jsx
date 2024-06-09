ce7698d3c57bf8fb74ef7a3a1a484c7c6f2626da
Bhuwan <hiphopbhuwan@gmail.com>
2024-06-10 09:01:28 +0930

    added rel=noreferrer in social.jsx after targer blank because of error

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/nautykid/?hl=en" className="home__social-icon" target="_blank">
+        <a href="https://www.instagram.com/nautykid/?hl=en" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-instagram"></i>
         </a>
 
-        <a href="https://dribbble.com/" className="home__social-icon" target="_blank">
+        <a href="https://dribbble.com/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-dribbble"></i>
             
         </a>
 
-        <a href="https://github.com/nautykid" className="home__social-icon" target="_blank">
+        <a href="https://github.com/nautykid" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-github-alt"></i>
             
         </a>
